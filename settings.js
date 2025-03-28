7f8e73ff17f388643ccdf48a65f97f1d9f1f0c52
rashidkhan3928 <148200016+rashidkhan3928@users.noreply.github.com>
2025-03-28 10:18:34 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "RK19",
+  botOwner: 'Rashid Khan', // Your name
+  ownerNumber: '923020396915', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
